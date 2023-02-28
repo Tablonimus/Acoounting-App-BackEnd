1eeab71b21577f582ab26c5f67ec5268b45a6a3e
tablonimus <lautamartin1995@gmail.com>
2023-02-28 15:31:16 -0300

    seeder

--- index.js
+++ index.js
@@ -19,9 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-// const {loadCountries}= require("../api/src/controllers/countryControl")
+
 // Syncing all the models at once.mcg
-//loadCountries() 
+
 conn.sync({ force: false  }).then(() => { 
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
